015153380cc47e5721ddf84476baba88ff9b0069
Mahari9 <merytseg77@gmail.com>
2024-01-02 08:50:49 -0800

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for llc in range(97, 123):
     if chr(llc) != 'q' and chr(llc) != 'e':
-        print("{}".format(chr(llc)), end=""
+        print("{}".format(chr(llc)), end="")
