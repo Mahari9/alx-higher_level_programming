6e96de2d30b3f7e2a45c459a63f6c2e0b33817ee
Mahari9 <merytseg77@gmail.com>
2024-02-06 10:13:27 -0800

    Class to JSON

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,1 +1,20 @@
-7-add_item.py
+#!/usr/bin/python3
+"""7-add_item
+    This script adds all arguments to a Python list and saves them to a file
+"""
+
+
+from sys import argv
+from os.path import exists
+
+
+if __name__ == "__main__":
+    save = __import__('5-save_to_json_file').save_to_json_file
+    load = __import__('6-load_from_json_file').load_from_json_file
+    f = "add_item.json"
+    if exists(f):
+        my_list = load(f)
+    else:
+        my_list = []
+    my_list.extend(argv[1:])
+    save(my_list, f)
