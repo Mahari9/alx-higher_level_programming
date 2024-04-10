175272254188f41c33fbf2e9fb5f87eacfcdd2a8
Mahari9 <merytseg77@gmail.com>
2024-04-10 10:01:25 -0700

    Code style

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,9 +1,14 @@
 #!/usr/bin/node
 class Rectangle {
-	constructor (w, h) {
-		this.width = w;
-		this.height = h;
-	}
+  /**
+   * Define an Rectangle with dimensions
+   * @param {Number} w The value of the width
+   * @param {Number} h The value of the height
+   */
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
 }
 
 module.exports = Rectangle;
